2253f001e86295e508c25315b1361cb94017a9a1
inv0w <jonjoninfante@gmail.com>
2019-09-29 15:52:07 -0700

    Added Procfile

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-client = MongoClient()
-db = client.Playlister
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 playlists = db.playlists
 
 app = Flask(__name__)
@@ -14,7 +15,7 @@ def index():
     return render_template('home.html', msg='Welcome to Playlister')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
 # OUR MOCK ARRAY OF PROJECTS
 # playlists = [
